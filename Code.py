08e2910a8f9a731a2900120db80f6bd81ba00e6b
TonyShark007 <mauritzlemgen@gmail.com>
2021-05-14 19:45:42 +0200

    Updated Code.py

--- Code.py
+++ Code.py
@@ -20,13 +20,38 @@ def universal():
         SP = I.split("# ")
         index = data['x'].index(str(SP[1]))
         print(data['y'][index])
+    elif I.startswith("*"):
+        SP = I.split("* ")
+        index = data['y'].index(str(SP[1]))
+        print(data['x'][index])
     elif I.startswith('let'):
         SP = I.split(" ")
         data['x'].append(SP[1])
         data['y'].append(SP[2])
         with open("Js.json", "w+") as f:
             json.dump(data, f, indent=4)
-        
-
+    elif I.startswith('if'):
+        SP = I.split(" ")
+        SPP = SP[1].split("=")
+        if SPP[0] in data['x']:
+            if SPP[1] in data['y']:
+                index = data['x'].index(SPP[0])
+                X = data['x'][index]
+                Y = data['y'][index]
+                if X == SPP[0] and Y == SPP[1]:
+                    if len(I) >= 15: 
+                        if SP[2] == "return":
+                            print(SP[3])
+                    else:
+                        print("True")
+                else:
+                    print("False")
+            else:
+                print("False")
+        else:
+            print("False")
+    elif I.startswith('println'):
+        SP = I.split(" ")
+        print(SP[1])
     
 universal()
